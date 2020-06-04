aee9c037a36bb0aa4d03987f985aaee9ee656d06
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-04 18:50:31 +0530

    Basic confsn model

---
+++ models/confessionModel.js
@@ -0,0 +1,53 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const confessionSchema=new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref:'users'
+    },
+    name:{
+        type:String,
+    },
+    text:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+        type:String
+    },
+    likes:[{
+        user:{
+          type:Schema.Types.ObjectId,
+          ref:'users'
+        }
+      }],
+    comments:[{
+        user:{
+          type:Schema.Types.ObjectId,
+          ref:'users'
+        },
+        text:{
+          type:String,
+          required:true
+        },
+        name:{
+          type:String
+        },
+        avatar:{
+          type:String
+        },
+        date:{
+          type:Date,
+          default:Date.now()
+        }
+      }],
+    date:{
+        type:Date,
+        default:Date.now()
+      }
+})
+
+const Confession=mongoose.model('confession',confessionSchema);
+
+module.exports=Confession;
