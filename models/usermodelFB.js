138371f8f17a69aeb487e5799177128de5a6ec93
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-04 15:05:33 +0530

    Fixed FBuser model
    :wq

--- models/usermodelFB.js
+++ models/usermodelFB.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
-const Schema=mongoose.Schema();
+const Schema=mongoose.Schema;
 
-const userschemaFB= new Schema({
+const userSchemaFB= new Schema({
         name: {
             type: String,
             required: true
